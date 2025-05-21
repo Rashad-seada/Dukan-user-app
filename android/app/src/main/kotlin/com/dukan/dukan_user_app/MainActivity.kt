f3d16ab90fc42bf497abdb0af06edc7caaa3bb7c
Rashad <rashadatef2@gmail.com>
2025-05-21 22:37:59 +0300

    init

--- android/app/src/main/kotlin/com/dukan/dukan_user_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.dukan.dukan_user_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/iraq/dukan_user_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.iraq.dukan_user_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() 
